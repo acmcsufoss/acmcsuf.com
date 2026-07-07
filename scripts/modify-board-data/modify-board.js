139b2286f6b42cb21ac94add80c2fdb3b5aec091
Gaballa <lGaballal@yahoo.com>
2026-07-07 01:08:01 -0700

    wip

--- scripts/modify-board-data/modify-board.js
+++ scripts/modify-board-data/modify-board.js
@@ -78,6 +78,95 @@ function findOfficer(boardOfficers, name) {
   return boardOfficers.find((o) => o.fullName.toLowerCase() === name.toLowerCase());
 }
 
+async function editOfficer(officers) {
+  const lookfor = await readInput('What officer do you want to edit/add?');
+  let officer = await findOfficer(officers, lookfor);
+  if (!officer) {
+    console.log('Unable to locate ', lookfor, '. Making new officer');
+
+    officer = {
+      fullName: lookfor
+        .split(' ')
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(' '),
+      picture: '',
+      discord: '',
+      positions: {},
+    };
+
+    officers.push(officer);
+  }
+
+  // Gaballa, why are you switching between string literal and template literal?
+  // because my lsp said so
+  while (true) {
+    const editPic = (await readInput("Edit this officer's picture link? [y/n]: ")).toLowerCase();
+    if (editPic === 'n' || editPic === 'no') {
+      break;
+    }
+
+    if (editPic === 'y' || editPic === 'yes') {
+      const picLink = await readInput('New picture link: ');
+      officer.picture = picLink;
+      break;
+    }
+
+    console.log('Invalid input, please try again');
+  }
+
+  while (true) {
+    const editDiscord = (await readInput("Edit this officer's discord? [y/n]: ")).toLowerCase();
+    if (editDiscord === 'n' || editDiscord === 'no') {
+      break;
+    }
+
+    if (editDiscord === 'y' || editDiscord === 'yes') {
+      const newDiscord = await readInput('New discord: ');
+      officer.discord = newDiscord;
+      break;
+    }
+
+    console.log('Invalid input, please try again');
+  }
+
+  while (true) {
+    const editPositions = (await readInput("Edit this officer's positions? [y/n]: ")).toLowerCase();
+
+    if (editPositions === 'n' || editPositions === 'no') {
+      break;
+    }
+
+    if (editPositions !== 'y' && editPositions !== 'yes') {
+      console.log('Invalid input, please try again.');
+      continue;
+    }
+
+    const term = (
+      await readInput('What term will you edit/add? (e.g. F25 or S26): ')
+    ).toUpperCase();
+
+    if (!officer.positions[term]) {
+      officer.positions[term] = [];
+    }
+
+    while (true) {
+      const title = await readInput('Officer title (leave blank to finish): ');
+
+      if (title === '') {
+        break;
+      }
+
+      officer.positions[term].push({
+        title,
+        tier: 0, // TODO: map tier to title with the json
+      });
+    }
+
+    break;
+  }
+
+  console.log(officer);
+}
 // =========== MAIN PROCESS ===========
 
 const rl = readline.createInterface(input, output);
@@ -85,6 +174,7 @@ const rl = readline.createInterface(input, output);
 const boardJSON = await getBoard();
 await writeToDiff(JSON.stringify(boardJSON));
 const boardOfficers = await getDiff();
+
 async function mainLoop() {
   while (true) {
     const option = await readInput(
@@ -112,6 +202,7 @@ async function mainLoop() {
         await readInput('Press any key to continue: ');
         break;
       case '2':
+        await editOfficer(boardOfficers);
         // todo
         break;
       case '3':
