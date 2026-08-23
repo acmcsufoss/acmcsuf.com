29f162abcd27cabd09bb41dd3076b2f4a6701743
bebopkenny <kennygarcia15@csu.fullerton.edu>
2026-08-23 13:45:36 -0700

    add fullyhacks 2026 description and image

--- src/routes/(site)/hackathons/data.ts
+++ src/routes/(site)/hackathons/data.ts
@@ -73,7 +73,8 @@ export default [
     date: 'April 2026',
     location: 'California State University, Fullerton',
     theme: 'Under the Sea',
-    description: 'TODO',
+    description:
+      'Back in the CS Building for another 24 hours, FullyHacks 2026 attracted over 380 applicants and 185 hackers, with 60 project submissions, 11 judges, and workshops led by OpenAI, Figma, Esri, and Human Delta.',
     directors: [
       { name: 'Max Rivas', picture: '/people/max-rivas.webp' },
       { name: 'Mark Ryan Garcia', picture: '/people/mark-ryan-garcia.webp' },
